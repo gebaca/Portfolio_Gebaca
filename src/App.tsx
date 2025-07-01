628d2de444b231719bed53fc6596d1f7404afbcb
Gerard Bataller <gebaca01@gmail.com>
2025-07-01 19:06:21 +0200

    frameworks and containers principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css';
 
 function App() {
-
   return (
     <>
+      <div className={styles.main_container_for_web_structure}>
+        <div className={styles.frame_container_for_web_elements}></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
