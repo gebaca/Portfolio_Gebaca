bc6b3bbd16c1ea271ea5bf254c6462e71409376a
Gerard Bataller <gebaca01@gmail.com>
2025-07-01 23:19:57 +0200

    feature/components-creation-web

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ function App() {
   return (
     <>
       <div className={styles.main_container_for_web_structure}>
-        <div className={styles.frame_container_for_web_elements}></div>
+        <div className={styles.frame_container_for_web_elements}>
+          <div className={styles.frame_container_for_web_elements__layout}>
+            <div className={styles.lateralMenu_nav_leftSide}></div>
+            <div
+              className={styles.separationSpace_between_leftSide_rightSide}
+            ></div>
+            <div className={styles.lateralMenu_info_rightSide}></div>
+          </div>
+        </div>
       </div>
     </>
   );
