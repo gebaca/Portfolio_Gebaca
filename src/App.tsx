9326fd7a0b703d1f54c345d92afe50eded2530f9
Gerard Bataller <gebaca01@gmail.com>
2025-07-06 17:54:14 +0200

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
       <div className={styles.main_container_for_web_structure}>
         <div className={styles.frame_container_for_web_elements}>
           <div className={styles.frame_container_for_web_elements__layout}>
-            <div className={styles.lateralMenu_nav_leftSide}>
+            <div
+              className={`${styles.lateralMenu_nav_leftSide} ${styles.lateral_menu_structure}`}
+            >
               <div className={styles.logo__component}>
                 <div
                   className={`${styles.name_logo__turcoise} ${styles.logo__size}`}
@@ -27,11 +29,19 @@ function App() {
                   designer & developer
                 </div>
               </div>
+              <ul className={styles.nav_links}>
+                <li className={styles.nav_item}>Me</li>
+                <li className={styles.nav_item}>Projects</li>
+                <li className={styles.nav_item}>Tools</li>
+                <li className={styles.nav_item}>Contact</li>
+              </ul>
             </div>
             <div
               className={styles.separationSpace_between_leftSide_rightSide}
             ></div>
-            <div className={styles.lateralMenu_info_rightSide}></div>
+            <div
+              className={`${styles.lateralMenu_info_rightSide} ${styles.lateral_menu_structure}`}
+            ></div>
           </div>
         </div>
       </div>
